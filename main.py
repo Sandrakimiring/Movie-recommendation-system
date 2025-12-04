7883488730a8d0c47cab2ac8aa1d8af438493465
Sandrakimiring <kimiringsandra@gmail.com>
2025-12-04 23:40:03 +0300

    added procfile

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.api.app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("src.api.app:app", host="0.0.0.0", port=port, reload=True)
+
+
+
